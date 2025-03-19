1381091b90f2396965b22ea38639ff436516251f
Andu <undoureche@gmail.com>
2025-03-19 22:50:39 +0200

    touched on felmyst

--- DBM-Sunwell/Felmyst.lua
+++ DBM-Sunwell/Felmyst.lua
@@ -17,7 +17,6 @@ mod:RegisterEventsInCombat(
 	)
 
 local warnEncaps		= mod:NewTargetAnnounce(45665, 4)
-local warnVapor			= mod:NewTargetAnnounce(45402, 3)
 local warnPhase			= mod:NewAnnounce("WarnPhase", 1, 31550)
 
 local specWarnGas		= mod:NewSpecialWarningSpell(45855, "Healer", nil, nil, 1, 2)
@@ -25,11 +24,11 @@ local specWarnCorrosion		= mod:NewSpecialWarningTaunt(45866, nil, nil, nil, 1, 2
 local specWarnEncaps		= mod:NewSpecialWarningYou(45665, nil, nil, nil, 1, 2)
 local yellEncaps		= mod:NewYell(45665)
 local specWarnEncapsNear	= mod:NewSpecialWarningClose(45665, nil, nil, nil, 1, 2)
-local specWarnVapor		= mod:NewSpecialWarningYou(45402, nil, nil, nil, 1, 2)
+local specWarnVapor		= mod:NewSpecialWarningSpell(45402, nil, nil, nil, 1, 2)
 local specWarnBreath		= mod:NewSpecialWarningCount(45717, nil, nil, nil, 3, 2)
 
 local timerGasCast		= mod:NewCastTimer(1, 45855)
-local timerGasCD		= mod:NewNextTimer(19, 45855, nil, nil, nil, 3)
+local timerGasCD		= mod:NewNextTimer(18, 45855, nil, nil, nil, 3)
 local timerCorrosion		= mod:NewTargetTimer(10, 45866, nil, "Tank", 2, 5, nil, DBM_COMMON_L.TANK_ICON)
 local timerEncaps		= mod:NewTargetTimer(7, 45665, nil, nil, nil, 3)
 local timerEncapsCD		= mod:NewNextTimer(18, 45665, nil, nil, nil, 3)
@@ -70,17 +69,30 @@ end
 function mod:OnCombatStart(delay)
 	self.vb.breathCounter = 0
 	self.vb.isFirstCleave = true
-	self.vb.groundingTime = GetTime()
+	self.vb.groundingTime = GetTime()-delay
 	
-	timerGasCD:Start(18-delay)
+	timerGasCD:Start(17-delay)
 	timerPhase:Start(-delay, L.Air)
 	berserkTimer:Start(-delay)
 	timerEncapsCD:Start(25-delay)
 end
 
+function mod:adjustGroundTimersIfFirstCast(realElapsed) 
+	if self.vb.isFirstCleave == true then
+		self.vb.isFirstCleave = false 
+		
+		local timerElapsed = GetTime() - self.vb.groundingTime
+		
+		timerGasCD:AddTime(-18+timerElapsed+18-realElapsed)
+		timerEncapsCD:AddTime(-25+timerElapsed+25-realElapsed)
+		timerPhase:AddTime(-60+timerElapsed+60-realElapsed, L.Air)
+	end
+end
 
 function mod:SPELL_AURA_APPLIED(args)
 	if args.spellId == 45866 then
+		self:adjustGroundTimersIfFirstCast(12)
+	
 		timerCorrosion:Start(args.destName)
 		if not args:IsPlayer() then -- validate
 			specWarnCorrosion:Show(args.destName)
@@ -91,12 +103,9 @@ end
 
 function mod:SPELL_SUMMON(args)
 	if args.spellId == 45392 then
-		if args.sourceName == UnitName("player") then
-			specWarnVapor:Show()
-			specWarnVapor:Play("targetyou")
-		else
-			warnVapor:Show(args.sourceName)
-		end
+		specWarnVapor:Show()
+		specWarnVapor:Play("runaway")
+		
 		if self.Options.VaporIcon then
 			self:SetIcon(args.sourceName, 8, 10)
 		end
@@ -106,7 +115,7 @@ end
 function mod:SPELL_CAST_START(args)
 	if args.spellId == 45855 then
 		timerGasCast:Start()
-		timerGasCD:Schedule(1)
+		timerGasCD:Schedule(1, 17)
 		specWarnGas:Show()
 		specWarnGas:Play("helpdispel")
 	end
@@ -118,7 +127,7 @@ function mod:Groundphase()
 	self.vb.groundingTime = GetTime()
 	
 	warnPhase:Show(L.Ground)
-	timerGasCD:Start(18)
+	timerGasCD:Start(17)
 	timerPhase:Start(60, L.Air)
 	timerEncapsCD:Start(25)
 end
@@ -132,8 +141,8 @@ function mod:CHAT_MSG_MONSTER_YELL(msg)
 		timerEncapsCD:Unschedule()
 		
 		timerBreath:Start(42, 1)
-		timerPhase:Start(86, L.Ground)
-		self:ScheduleMethod(86, "Groundphase")
+		timerPhase:Start(104, L.Ground)
+		self:ScheduleMethod(104, "Groundphase")
 	end
 end
 
@@ -149,13 +158,8 @@ function mod:CHAT_MSG_RAID_BOSS_EMOTE(msg)
 end
 
 function mod:UNIT_SPELLCAST_SUCCEEDED(_, spellName)
-	if spellName == GetSpellInfo(19983) and self.vb.isFirstCleave == true then
-		self.vb.isFirstCleave = false
-		
-		local timerDelta = (GetTime() - self.vb.groundingTime) - 8
-		timerGasCD:AddTime(timerDelta)
-		timerEncapsCD:AddTime(timerDelta)
-		timerPhase:AddTime(timerDelta, L.Air)
+	if spellName == GetSpellInfo(19983) then
+		self:adjustGroundTimersIfFirstCast(8)
 		
 	elseif spellName == GetSpellInfo(45661) and self:AntiSpam(2, 1) then
 		self:BossTargetScanner(25038, "EncapsulateTarget", 0.05, 10)
