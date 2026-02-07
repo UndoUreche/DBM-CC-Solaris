c0e35e1cfa152a5cf949b3b8c0dcf1ac9de26a57
Undo <undoureche@gmail.com>
2026-02-07 20:56:33 +0200

    arachnid quarter

--- DBM-Naxx/ArachnidQuarter/Anub'Rekhan.lua
+++ DBM-Naxx/ArachnidQuarter/Anub'Rekhan.lua
@@ -12,28 +12,21 @@ mod:RegisterEventsInCombat(
 	"SPELL_AURA_REMOVED 28785 54021"
 )
 
-local warningLocustSoon		= mod:NewSoonAnnounce(28785, 2)
-local warningLocustFaded	= mod:NewFadesAnnounce(28785, 1)
+local warningLocustFaded		= mod:NewFadesAnnounce(28785, 1)
 local warnImpale			= mod:NewTargetNoFilterAnnounce(28783, 3, nil, false)
 
-local specialWarningLocust	= mod:NewSpecialWarningSpell(28785, nil, nil, nil, 2, 2)
+local specialWarningLocust		= mod:NewSpecialWarningSpell(28785, nil, nil, nil, 2, 2)
 local yellImpale			= mod:NewYell(28783, nil, false)
 
-local timerLocustIn			= mod:NewCDTimer(80, 28785, nil, nil, nil, 6)
-local timerLocustFade		= mod:NewBuffActiveTimer(23, 28785, nil, nil, nil, 6)
-local timerImpale			= mod:NewCDTimer(13, 56090, nil, nil, nil, 3) -- REVIEW! ~7s variance [13.0-19.8]? (25m Lordaeron 2022/10/16) -- 13.7, 13.6, 19.8, 13.0
+local timerLocustIn			= mod:NewCDTimer(67, 28785, nil, nil, nil, 6)
+local timerLocustFade			= mod:NewBuffActiveTimer(23, 28785, nil, nil, nil, 6)
+local timerImpale			= mod:NewNextTimer(20, 28783, nil, nil, nil, 3)
 
-mod:AddBoolOption("ArachnophobiaTimer", true, "timer", nil, nil, nil, "at1859")--Sad caveat that 10 and 25 man have own achievements and we have to show only 1 in GUI
+mod:AddBoolOption("ArachnophobiaTimer", true, "timer", nil, nil, nil, "at1859")
 
 function mod:OnCombatStart(delay)
-	if self:IsDifficulty("normal25") then
-		timerLocustIn:Start(100 - delay)
-		warningLocustSoon:Schedule(90 - delay)
-	else
-		timerLocustIn:Start(91 - delay)
-		warningLocustSoon:Schedule(76 - delay)
-	end
-	timerImpale:Start(12.7-delay) -- REVIEW! variance? (25m Lordaeron 2022/10/16) - pull:12.7
+	timerLocustIn:Start(70 - delay)
+	timerImpale:Start(15-delay)
 end
 
 function mod:OnCombatEnd(wipe)
@@ -46,19 +39,16 @@ function mod:SPELL_CAST_START(args)
 	if args:IsSpellID(28785, 54021) then  -- Locust Swarm
 		specialWarningLocust:Show()
 		specialWarningLocust:Play("aesoon")
-		timerLocustIn:Stop()
-		if self:IsDifficulty("normal25") then
-			timerLocustFade:Start(23)
-		else
-			timerLocustFade:Start(19)
-		end
+		timerLocustIn:Stop()	
+		timerLocustFade:Start(23)
 	end
 end
 
 function mod:SPELL_CAST_SUCCESS(args)
-	if args:IsSpellID(28783, 56090) then  -- Impale. REVIEW! 28783 needed?
+	if args:IsSpellID(28783, 56090) then 
 		timerImpale:Start()
 		warnImpale:Show(args.destName)
+		
 		if args:IsPlayer() then
 			yellImpale:Yell()
 		end
@@ -70,6 +60,5 @@ function mod:SPELL_AURA_REMOVED(args)
 	and args.auraType == "BUFF" then
 		warningLocustFaded:Show()
 		timerLocustIn:Start()
-		warningLocustSoon:Schedule(62)
 	end
 end
--- DBM-Naxx/ArachnidQuarter/Maexxna.lua
+++ DBM-Naxx/ArachnidQuarter/Maexxna.lua
@@ -6,22 +6,20 @@ mod:SetCreatureID(15952)
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
-	"SPELL_AURA_APPLIED 28622",
-	"SPELL_CAST_SUCCESS 29484 54125"
+	"SPELL_AURA_APPLIED 28622 29484 54125"
 )
-
---TODO, verify nax40 web wrap timer
+	
 local warnWebWrap		= mod:NewTargetNoFilterAnnounce(28622, 2)
-local warnWebSpraySoon	= mod:NewSoonAnnounce(29484, 1)
-local warnWebSprayNow	= mod:NewSpellAnnounce(29484, 3)
-local warnSpidersSoon	= mod:NewAnnounce("WarningSpidersSoon", 2, 17332)
-local warnSpidersNow	= mod:NewAnnounce("WarningSpidersNow", 4, 17332)
+local warnWebSpraySoon		= mod:NewSoonAnnounce(29484, 1)
+local warnWebSprayNow		= mod:NewSpellAnnounce(29484, 3)
+local warnSpidersSoon		= mod:NewAnnounce("WarningSpidersSoon", 2, 17332)
+local warnSpidersNow		= mod:NewAnnounce("WarningSpidersNow", 4, 17332)
 
-local specWarnWebWrap	= mod:NewSpecialWarningSwitch(28622, "RangedDps", nil, nil, 1, 2)
+local specWarnWebWrap		= mod:NewSpecialWarningSwitch(28622, "RangedDps", nil, nil, 1, 2)
 local yellWebWrap		= mod:NewYellMe(28622)
 
 local timerWebSpray		= mod:NewNextTimer(40, 29484, nil, nil, nil, 2)
-local timerWebWrap		= mod:NewNextTimer(39.6, 28622, nil, "RangedDps|Healer", nil, 3)-- 39.593-40.885
+local timerWebWrap		= mod:NewNextTimer(40, 28622, nil, "RangedDps|Healer", nil, 3)-- 39.593-40.885
 local timerSpider		= mod:NewTimer(30, "TimerSpider", 17332, nil, nil, 1)
 
 local function Spiderlings(self)
@@ -35,7 +33,8 @@ end
 function mod:OnCombatStart(delay)
 	warnWebSpraySoon:Schedule(35 - delay)
 	timerWebSpray:Start(40 - delay)
-	timerWebWrap:Start(20.1 - delay)--20.095-21.096
+	timerWebWrap:Start(20 - delay)
+
 	warnSpidersSoon:Schedule(25 - delay)
 	warnSpidersNow:Schedule(30 - delay)
 	timerSpider:Start(30 - delay)
@@ -53,18 +52,15 @@ end
 function mod:SPELL_AURA_APPLIED(args)
 	if args.spellId == 28622 then -- Web Wrap
 		warnWebWrap:CombinedShow(0.5, args.destName)
+		timerWebWrap:Start()
+
 		if args.destName == UnitName("player") then
 			yellWebWrap:Yell()
 		elseif not DBM:UnitDebuff("player", args.spellName) and self:AntiSpam(3, 1) then
 			specWarnWebWrap:Show()
 			specWarnWebWrap:Play("targetchange")
-			timerWebWrap:Start()
 		end
-	end
-end
-
-function mod:SPELL_CAST_SUCCESS(args)
-	if args:IsSpellID(29484, 54125) then -- Web Spray
+	elseif args:IsSpellID(29484, 54125) and self:AntiSpam(5, 2) then -- Web Spray
 		warnWebSprayNow:Show()
 		warnWebSpraySoon:Schedule(35)
 		timerWebSpray:Start()
