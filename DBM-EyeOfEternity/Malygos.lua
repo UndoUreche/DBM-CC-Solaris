aa46e39fa59d118fcf95311cf81e429ac63dd91e
Undo <undoureche@gmail.com>
2026-03-14 20:46:31 +0200

    malygos. Static field is broken on the server so we can't track it (not even as a script, too many random timers and movement)

--- DBM-EyeOfEternity/Malygos.lua
+++ DBM-EyeOfEternity/Malygos.lua
@@ -13,23 +13,24 @@ mod:RegisterEvents(
 )
 
 mod:RegisterEventsInCombat(
-	"SPELL_AURA_APPLIED 60936 57407",
+	"SPELL_AURA_APPLIED 60936 57407 55853",
 	"SPELL_CAST_START 56505",
-	"SPELL_CAST_SUCCESS 56105 57430",
+	"SPELL_CAST_SUCCESS 57430",
 	"CHAT_MSG_RAID_BOSS_EMOTE",
-	"UNIT_SPELLCAST_SUCCEEDED boss1"
+	"UNIT_THREAT_LIST_UPDATE",
+	"TRACKED_ACHIEVEMENT_UPDATE"
 )
 -- General
-local enrageTimer				= mod:NewBerserkTimer(615)
+local enrageTimer				= mod:NewBerserkTimer(600)
 local timerAchieve				= mod:NewAchievementTimer(360, 1875)
 
 -- Stage One
 mod:AddTimerLine(DBM_CORE_L.SCENARIO_STAGE:format(1))
-local warnSummonPowerSpark		= mod:NewSpellAnnounce(56140, 2, 59381)
+local warnSummonPowerSpark			= mod:NewSpellAnnounce(56140, 2, 59381)
 local warnVortex				= mod:NewSpellAnnounce(56105, 3)
-local warnVortexSoon			= mod:NewSoonAnnounce(56105, 2)
+local warnVortexSoon				= mod:NewSoonAnnounce(56105, 2)
 
-local timerSummonPowerSpark		= mod:NewNextTimer(30, 56140, nil, nil, nil, 1, 59381, DBM_COMMON_L.DAMAGE_ICON) -- (10man Lordaeron 2022/09/27 || 25man Lordaeron 2022/09/27) - 30.0 || 30.0, 30.0
+local timerSummonPowerSpark			= mod:NewCDTimer(20, 56140, nil, nil, nil, 1, 59381, DBM_COMMON_L.DAMAGE_ICON)
 local timerVortex				= mod:NewCastTimer(11, 56105, nil, nil, nil, 5, nil, DBM_COMMON_L.HEALER_ICON)
 local timerVortexCD				= mod:NewNextTimer(60, 56105, nil, nil, nil, 2)
 
@@ -38,29 +39,28 @@ mod:AddTimerLine(DBM_CORE_L.SCENARIO_STAGE:format(2))
 local warnPhase2				= mod:NewPhaseAnnounce(2)
 local warnBreathInc				= mod:NewSoonAnnounce(56505, 3)
 
-local specWarnBreath			= mod:NewSpecialWarningSpell(56505, nil, nil, nil, 2, 2)
+local specWarnBreath				= mod:NewSpecialWarningSpell(56505, nil, nil, nil, 2, 2)
 
-local timerBreath				= mod:NewBuffActiveTimer(8, 56505, nil, nil, nil, 5) --lasts 5 seconds plus 3 sec cast.
-local timerBreathCD				= mod:NewCDTimer(59, 56505, nil, nil, nil, 2)
-local timerIntermission			= mod:NewPhaseTimer(22)
+local timerBreath				= mod:NewBuffActiveTimer(8, 56505, nil, nil, nil, 5)
+local timerBreathCD				= mod:NewNextTimer(65, 56505, nil, nil, nil, 2)
+local timerIntermission				= mod:NewPhaseTimer(24)
 
 -- Stage Three
 mod:AddTimerLine(DBM_CORE_L.SCENARIO_STAGE:format(3))
 local warnPhase3				= mod:NewPhaseAnnounce(3)
+
 local warnSurge					= mod:NewTargetAnnounce(60936, 3)
-local warnStaticField			= mod:NewTargetNoFilterAnnounce(57430, 3)
 
 local specWarnSurge				= mod:NewSpecialWarningDefensive(60936, nil, nil, nil, 1, 2)
-local specWarnP3SurgeOfPowerSoon= mod:NewSpecialWarningYou(60936, nil, nil, nil, 1, 2)
-local specWarnStaticField		= mod:NewSpecialWarningYou(57430, nil, nil, nil, 1, 2)
-local specWarnStaticFieldNear	= mod:NewSpecialWarningClose(57430, nil, nil, nil, 1, 2)
-local yellStaticField			= mod:NewYellMe(57430)
+--local specWarnStaticField			= mod:NewSpecialWarningYou(57430, nil, nil, nil, 1, 2)
+--local specWarnStaticFieldNear			= mod:NewSpecialWarningClose(57430, nil, nil, nil, 1, 2)
 
-local timerStaticFieldCD		= mod:NewCDTimer(10, 57430, nil, nil, nil, 3, nil, nil, true) --REVIEW! ~10s variance? Added "keep" arg (10man Lordaeron 2022/09/27 || 25man Lordaeron 2022/09/27) - 10.0, 16.2, 11.6, 10.2, 10.4 || 15.1, 20.1, 13.8, 19.5, 19.6, 11.4, 18.5
---local timerAttackable			= mod:NewTimer(24, "Malygos Wipes Debuffs") -- Not enough info nor locales on the code from previous contributor to know what this is intended for. Disabled for now
+--local timerStaticFieldCD			= mod:NewCDTimer(13, 57430, nil, nil, nil, 3, nil, nil, true)
 
 local tableBuild = false
 local guids = {}
+
+local hasStarted = false
 
 local function buildGuidTable()
 	table.wipe(guids)
@@ -93,36 +93,56 @@ function mod:StaticFieldTarget()
 end
 
 function mod:OnCombatStart(delay)
-	tableBuild = false
 	self:SetStage(1)
-	enrageTimer:Start(-delay)
-	timerAchieve:Start(-delay)
-	timerVortexCD:Start(38.7) -- REVIEW! ~4s variance? (10man Lordaeron 2022/09/27 || 25man Lordaeron 2022/09/27) - pull:42.6/Stage 1/42.6 || pull:38.8/Stage 1/38.8 (but debug gave 1.3 delta)
-	timerSummonPowerSpark:Start(28.1-delay) -- REVIEW! ~3s variance? (10man Lordaeron 2022/09/27 || 25man Lordaeron 2022/09/27) - pull:31.4/Stage 1/31.4 || pull:28.1/Stage 1/28.1
 	table.wipe(guids)
+
+	tableBuild = false
+	hasStarted = false
+end
+
+function mod:TRACKED_ACHIEVEMENT_UPDATE(id)
+
+	if not hasStarted and id == 1875 or id == 1874 then
+		self:SendSync("Start", 0)
+	end
+end
+
+function mod:UNIT_THREAT_LIST_UPDATE(uid)
+
+	if not hasStarted and DBM:GetUnitCreatureId("target") == 28859 and UnitCanAttack("player", "target") then
+		self:SendSync("Start", 0)
+	end
+end
+
+function mod:SPELL_CAST_SUCCESS(args)
+	local spellId = args.spellId
+
+	if spellId == 57430 then
+		DBM:AddMsg("Ping Wizs, the big spark can be tracked!")
+		--self:ScheduleMethod(0.1, "StaticFieldTarget")
+		--timerStaticFieldCD:Start()
+	end
 end
 
 function mod:SPELL_AURA_APPLIED(args)
 	if args:IsSpellID(60936, 57407) then
-		DBM:Debug("SURGE" .. guids[args.destGUID], 2)
 		local target = guids[args.destGUID or 0]
 		if target then
-			warnSurge:CombinedShow(0.5, target)
 			if target == UnitName("player") then
 				specWarnSurge:Show()
 				specWarnSurge:Play("defensive")
+			else
+				warnSurge:CombinedShow(0.5, target)
 			end
 		end
+	elseif args:IsSpellID(55853) then
+		self:SendSync("Vortex")
 	end
 end
 
--- not really sure which one this spell is casted by. Use both i guess
 function mod:SPELL_CAST_START(args)
-	local spellId = args.spellId
-	if self:GetCIDFromGUID(args.sourceGUID) == 28859 then
-		DBM:Debug("SCStart " .. spellId .. GetSpellLink(spellId) , 2)
-	end
-	if spellId == 56505 then--His deep breath
+	
+	if args.spellId == 56505 then--His deep breath
 		specWarnBreath:Show()
 		specWarnBreath:Play("findshield")
 		timerBreath:Start()
@@ -130,87 +150,91 @@ function mod:SPELL_CAST_START(args)
 	end
 end
 
-function mod:SPELL_CAST_SUCCESS(args)
-	local spellId = args.spellId
-	if self:GetCIDFromGUID(args.sourceGUID) == 28859 then
-		DBM:Debug("SCSuccess " .. spellId .. GetSpellLink(spellId) , 2)
-	end
-	if spellId == 56105 then
-		timerVortexCD:Start()
-		warnVortexSoon:Schedule(54)
-		warnVortex:Show()
-		timerVortex:Start()
-		-- Commenting this block out, since Sparks are fixed on a 30s interval... no need to correct anything on the fly
---		if timerSummonPowerSpark:GetTime() < 11 and timerSummonPowerSpark:IsStarted() then
---			timerSummonPowerSpark:Update(18, 30)
---		end
-	elseif spellId == 57430 then
-		self:ScheduleMethod(0.1, "StaticFieldTarget")
-		--warnStaticField:Show()
-		timerStaticFieldCD:Start()
-	end
-end
-
 function mod:CHAT_MSG_MONSTER_YELL(msg)
-	--Secondary pull trigger, so we can detect combat when he's pulled while already in combat (which is about 99% of time)
 	if (msg == L.YellPull or msg:find(L.YellPull)) and not self:IsInCombat() then
 		DBM:StartCombat(self, 0)
-	elseif msg:sub(0, L.YellPhase2:len()) == L.YellPhase2 then
+	elseif msg:sub(0, L.YellPhase1End:len()) == L.YellPhase1End then
 		self:SendSync("Phase2")
+	elseif msg == L.YellPhase2 or msg:find(L.YellPhase2) then
+		self:SendSync("Phase2Start")
 	elseif msg == L.YellBreath or msg:find(L.YellBreath) then
 		self:SendSync("BreathSoon")
+	elseif msg == L.EnoughScream then
+		self:SendSync("Phase2End")
 	elseif msg:sub(0, L.YellPhase3:len()) == L.YellPhase3 then
 		self:SendSync("Phase3")
+	elseif msg == L.YellVortex then 
+		--vortex yell message triggers a delay on all abilities beside berserk
+		if timerSummonPowerSpark:IsStarted() then
+			timerSummonPowerSpark:AddTime(25)
+		else
+			timerSummonPowerSpark:Start(25)
+		end
 	elseif msg == L.EnoughScream then
 		timerBreathCD:Stop()
---		timerAttackable:Start()
-		timerStaticFieldCD:Start(24+15.5)
 	end
 end
 
 function mod:CHAT_MSG_RAID_BOSS_EMOTE(msg)
---	if msg == L.EmoteSpark or msg:find(L.EmoteSpark) then
---		self:SendSync("Spark")
-	if msg == L.EmoteSurge or msg:find(L.EmoteSurge) then
+	if msg == L.EmoteSpark or msg:find(L.EmoteSpark) then
+		self:SendSync("Spark")
+	elseif msg == L.EmoteSurge or msg:find(L.EmoteSurge) then
 		self:SendSync("MalygosSurge", UnitName("player"))
 	end
 end
 
---localization free triggers that's better but can only be used where boss1 UnitId available
-function mod:UNIT_SPELLCAST_SUCCEEDED(_, spellName)
---	"<39.8> [UNIT_SPELLCAST_SUCCEEDED] Malygos:Possible Target<Omegal>:target:Summon Power Spark::0:56140", -- [998]
-	if spellName == GetSpellInfo(56140) then
-		warnSummonPowerSpark:Show()
-		timerSummonPowerSpark:Start()
-	end
-end
 
 function mod:OnSync(event, arg)
 	if not self:IsInCombat() then return end
---	if event == "Spark" then
---		warnSummonPowerSpark:Show()
---		timerSummonPowerSpark:Start()
-	if event == "Phase2" then
+	
+	--encounter start trackers are unreliable, therefore the 3 methods of tracking it are used, merged here
+	if event == "Start" and self:AntiSpam() then --arg is a dealy
+		hasStarted = true
+
+		enrageTimer:Start(-arg)
+		timerVortexCD:Start(30-arg)
+		timerSummonPowerSpark:Start(10-arg)
+		timerAchieve:Start(-arg)
+
+	-- vortex sync to avoid issues with not everyone being affected
+	elseif event == "Vortex" and self:AntiSpam() then 
+		warnVortex:Show()
+		timerVortex:Start()
+
+		timerVortexCD:Start(72)
+		warnVortexSoon:Schedule(67)
+
+	elseif event == "Spark" then
+		warnSummonPowerSpark:Show()
+		timerSummonPowerSpark:Start()
+
+	elseif event == "Phase2" then
 		self:SetStage(2)
+
+		warnPhase2:Show()
+
 		timerSummonPowerSpark:Cancel()
 		timerVortexCD:Cancel()
 		warnVortexSoon:Cancel()
-		warnPhase2:Show()
+		
 		timerIntermission:Start()
-		timerBreathCD:Start(67.9) -- REVIEW! no variance? (10man Lordaeron 2022/09/27 || 25man Lordaeron 2022/09/27) - Stage 2/68.0 || Stage 2/68.0
+
+	elseif event == "Phase2Start" then
+		timerBreathCD:Start(55)
+
 	elseif event == "BreathSoon" then
 		warnBreathInc:Show()
+
+	elseif event == "Phase2End" then
+		timerBreathCD:Cancel()
+
 	elseif event == "Phase3" then
 		self:SetStage(3)
 		warnPhase3:Show()
+
 		self:Schedule(6, buildGuidTable)
-		timerBreathCD:Cancel()
-		timerStaticFieldCD:Start(20.2) -- REVIEW! ~4s variance? (10man Lordaeron 2022/09/27 || 25man Lordaeron 2022/09/27) - Stage 3/24.5 || Stage 3/20.2
-	elseif event == "MalygosSurge" then
-		warnSurge:CombinedShow(0.2, arg)
-		if arg == UnitName("player") then
-			specWarnP3SurgeOfPowerSoon:Show()
-			specWarnP3SurgeOfPowerSoon:Play("findshield")
-		end
+		
+		--timerBreathCD:Cancel()
+		--timerStaticFieldCD:Start(20.2)
 	end
 end
--- DBM-EyeOfEternity/localization.en.lua
+++ DBM-EyeOfEternity/localization.en.lua
@@ -12,10 +12,12 @@ L:SetGeneralLocalization({
 L:SetMiscLocalization({
 	YellPull	= "My patience has reached its limit. I will be rid of you!",
 	EmoteSpark	= "A Power Spark forms from a nearby rift!",
-	YellPhase2	= "I had hoped to end your lives quickly",
+	YellPhase1End	= "I had hoped to end your lives quickly",
+	YellPhase2	= "Few have experienced the pain I will now inflict upon you!",
 --	EmoteBreath	= "%s takes a deep breath.",
 	YellBreath	= "You will not succeed while I draw breath!",
 	YellPhase3	= "Now your benefactors make their",
---	EnoughScream	= "ENOUGH! If you intend to reclaim Azeroth's magic, then you shall have it!"
-	EmoteSurge	= "%s fixes his eyes on you!"
+	EnoughScream	= "ENOUGH! If you intend to reclaim Azeroth's magic, then you shall have it!",
+	EmoteSurge	= "%s fixes his eyes on you!",
+	YellVortex	= "Watch helplessly as your hopes are swept away!"
 })
